3572dc627e260c2f70d256485b3500151782da86
Binh Van Nguyen <binhvnguyen@kms-technology.com>
2018-11-21 11:09:23 +0700

    Basic finish. Need refactor code and add layout.

--- simple-app/simple-app/simple-app/FilmListScreen.swift
+++ simple-app/simple-app/simple-app/FilmListScreen.swift
@@ -12,18 +12,76 @@ import AlamofireImage
 
 class FilmListScreen: UIViewController {
     
+    let URL_GET_DATA = "http://www.mocky.io/v2/5bf3bce23100002c00619909"
+    
+    let HOST_IMAGE = "http://image.tmdb.org/t/p/w500"
+    
     @IBOutlet weak var filmTable: UITableView!
-    var films: [Film] = []
+    var films = [Film]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        films = createArray()
+        createDataCompletion(completion: {
+            (filmsTemp) in
+            self.films = filmsTemp
+            self.filmTable.reloadData()
+        })
     }
     
-    func createArray() -> [Film] {
-        var filmsTemp: [Film] = []
-        let film1 = Film(image: #imageLiteral(resourceName: "img1"), title: "Pic 1")
-        filmsTemp.append(film1)
+    fileprivate func getFilmObject(_ jsonArray: [[String : Any]], _ i: Int) -> Film {
+        let filmData = jsonArray[i]
+        let filmTitle: String = "\(String(describing: filmData["title"]!))"
+        let filmUrl: String = self.HOST_IMAGE+"\(String(describing: filmData["poster_path"]!))"
+        return Film(image: self.getImage(filmUrl)!, title: filmTitle)
+    }
+    
+    fileprivate func getFilmsArray(_ json: Any) -> [Film] {
+        var filmsTemp = [Film]()
+        let data = "\(json)".data(using: .utf8)!
+        do {
+            if let jsonArray = try JSONSerialization.jsonObject(with: data, options : .allowFragments) as? [Dictionary<String,Any>]
+            {
+                for i in 0..<jsonArray.count {
+                    let film = self.getFilmObject(jsonArray, i)
+                    filmsTemp.append(film)
+                }
+            } else {
+                print("Bad Json")
+            }
+        } catch let error as NSError {
+            print(error)
+        }
         return filmsTemp
+    }
+    
+    func createDataCompletion(completion:@escaping (_ filmsTemp: [Film]) ->()) {
+        var filmsTemp = [Film]()
+        Alamofire.request(URL_GET_DATA).responseJSON { response in
+            switch(response.result) {
+            case .success(_):
+                if let json = response.result.value {
+                    filmsTemp = self.getFilmsArray(json)
+                }
+                completion(filmsTemp)
+                break
+            case .failure(_):
+                completion(filmsTemp)
+                break;
+            }
+        }
+        
+    }
+    
+    func getImage(_ imageUrl: String) -> UIImage? {
+        if let url = URL(string: imageUrl) {
+            do {
+                let data = try Data(contentsOf: url)
+                return UIImage(data: data)
+            } catch let err {
+                print("Error: \(err.localizedDescription)")
+            }
+        }
+        return UIImage()
     }
 }
 
